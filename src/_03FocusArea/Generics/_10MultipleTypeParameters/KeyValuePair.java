c2e88e29f251bb65c34a9a02dcb323158d2be871
gra-m <91182298+gra-m@users.noreply.github.com>
2022-05-20 14:50:32 +0200

    closes #7 finally, multiple type params at class level

---
+++ src/_03FocusArea/Generics/_10MultipleTypeParameters/KeyValuePair.java
@@ -0,0 +1,17 @@
+package _03FocusArea.Generics._10MultipleTypeParameters;
+
+/**
+ * Created by Gra_m on 2022 05 20
+ */
+
+public class KeyValuePair <K, V>{
+	private K key;
+	private V value;
+
+	public KeyValuePair(K key, V value){
+		this.key=key;
+		this.value=value;
+	}
+
+
+}
