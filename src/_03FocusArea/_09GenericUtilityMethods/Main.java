9925ba7f2a3d35b0ec29984ad2ec90786a2fab58
gra-m <91182298+gra-m@users.noreply.github.com>
2022-05-20 13:51:45 +0200

    #6 before generic

---
+++ src/_03FocusArea/_09GenericUtilityMethods/Main.java
@@ -0,0 +1,12 @@
+package _03FocusArea._09GenericUtilityMethods;
+
+
+/**
+ * Created by Gra_m on 2022 05 18
+ */
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/_03FocusArea/_09GenericUtilityMethods/Utils.java
@@ -0,0 +1,13 @@
+package _03FocusArea._09GenericUtilityMethods;
+
+/**
+ * Created by Gra_m on 2022 05 20
+ */
+
+public class Utils {
+
+	public static int max(int first, int second) {
+		// ;) replace with Math.max() call
+		return (first > second) ? first:second;
+	}
+}
